1955dc433ec26bb26ddd47e43ff3cde9492605fa
Hsun <hsun.chen@walkermedia.tw>
2021-11-15 17:40:35 +0800

    add ics file generator

--- ics/urls.py
+++ ics/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('generate', views.generate_ics_file, name='generate')
 ]
--- ics/views.py
+++ ics/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+from icalendar import Calendar, Event
+from datetime import datetime, timezone, timedelta
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
 def index(request):
+    if request.method != 'GET':
+        return  HttpResponse('method not allowed.', content_type='application/json', status=405)
+
     return render(request, 'ics/index.html')
+
+def generate_ics_file(request):
+    if request.method != 'POST':
+        return  HttpResponse('method not allowed.', content_type='application/json', status=405)
+
+    post_data = request.POST
+
+    cal = Calendar()
+    cal.add('prodid', '-//ics generator//')
+    cal.add('version', '2.0')
+
+    event = Event()
+    event.add('summary', post_data.get('summary'))
+    event.add('description', post_data.get('description'))
+    event.add('dtstart', datetime.strptime(post_data.get('startTime'), '%Y-%m-%dT%H:%M'))
+    event.add('dtend', datetime.strptime(post_data.get('endTime'), '%Y-%m-%dT%H:%M'))
+    event.add('dtstamp', datetime.now())
+
+    cal.add_component(event)
+
+    response = HttpResponse(cal.to_ical(), content_type='text/calendar')
+    response['Content-Disposition'] = 'attachment; filename=event.ics'
+
+    return response
